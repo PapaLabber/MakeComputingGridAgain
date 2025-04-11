d2ef2891da782fa63d91d61f395bad021d3b485e
philippeysl20 <rd57yh@student.aau.dk>
2025-04-11 15:12:37 +0200

    Update TaskBroker.js

--- node/TaskBroker.js
+++ node/TaskBroker.js
@@ -9,16 +9,23 @@ class task {
 class node { // Nodes in the linked list
     constructor(task) {
         this.task = task; // Current task
-        this.timeStamp; // Time when the task was created
         this.next = null; // Pointer to the next task
         this.prev = null; // Pointer to the previous task
+        this.timeStamp; // Time when the task was created
     }
+    
 }
-
 class messageQueue { // The queue itself
     constructor() {
         this.head; // The front node in the queue
         this.tail; // The back node in the queue
+    }
+}
+
+class dqList { // The dequeued list.
+    constructor() {
+        this.head; // The front node of the dequeued list
+        this.tail; // The back node of the dequeued list
     }
 }
 
@@ -37,46 +44,65 @@ function enqueue(queue, task) {
 }
 
 // Make a function that removes a task from the list (dequeue)
-function dequeue(queue) {
-    if (!queue.head.task) { // If queue empty
+function dequeue(mq, dq) {
+    if (!mq.head) { // If queue empty
         console.log("Queue is empty");
         return null;
     }
 
-    queue.head.timeStamp = Date.now(); // Set the time stamp of the head node for when it is dequeued
-    const removedTask = queue.head.task; // Get the task from the head
+    const newdqNode = new Node(mq.head.task); // Make a node in dqList
+    newdqNode.experationTime = Date.now()+60000; // Set an experation to signal time for requeue.
 
-    if (queue.head === queue.tail) { // If there is only one node in the queue
-        queue.head.task = null; // Set the head task to null to keep pointers
-        queue.tail.task = null; // Set the tail task to null to keep pointers
-        return removedTask;
+    if (mq.head === mq.tail) { // If there is only one node in the message queue
+        mq.head.task = null; // Set the head task to null to keep pointers
+        mq.tail.task = null; // Set the tail task to null to keep pointers
     }
     else {
-        queue.head = queue.head.next; // Move the head to the next node
-        return removedTask;
+        mq.head = mq.head.next; // Move the head to the next node
+        mq.head.prev.next = null; // Remove pointers from the previous head.
+        mq.head.prev = null; // Remove pointer to the previous head.
     }
-}
-// Make a function that views the task at the front of the list without removing it (peek)
-function peek() {
 
+    if (!dq.head) { // If dequeue list is empty
+        dq.head = newdqNode;
+        dq.tail = newdqNode;
+    } else { // If dequeue list is not empty
+        dq.tail.next = newdqNode; // Set the next of the tail to the new node
+        newdqNode.prev = dq.tail; // Set the previous of the new node to the tail
+        dq.tail = newdqNode; // Set the tail to the new node
+    }
+    console.log(`Task ${newdqNode.task.id} has been dequeued and is now stored in DEEEEEEEEEEZ NUTZZZZZZZZZZ`) // Check if group is reading our code.
+    // "DEEEEEEEEEEZ NUTZZZZZZZZZZ" should be changed to "dequeue list" once the joke has been finished.
 }
 
 // A function that marks a task as done/finished when it has computed it fully (acknowledge)
 // The function takes two parametres. The queue itself and the ID of the task we are looking for.
 function acknowledge(queue, taskId) {
-    if (!queue.head.task) { // If queue empty
-        console.log("Queue is empty");
+    if (!queue.head) { // If queue empty
+        console.log("Dequeue list is empty");
         return null;
     }
 
-    let currentTask = queue.head.prev;
-    while (currentTask) {
-        if (currentTask.id === taskId) { // Check if current task ID is the same as the ID we are looking for
-            deleteNode(currentTask); // Delete the task from the queue
+    let targetNode = queue.head;
+    while (targetNode) {
+        if (targetNode.id === taskId) { // Check if current task ID is the same as the ID we are looking for
+            if (targetNode === queue.head) { // Check if target node is the head.
+                targetNode.next.prev = null; // Nuke only pointers to the next node, because target node is first.
+                targetNode.next = null;
+            } else if (targetNode === queue.tail) { // Check if target node is the tail.
+                targetNode.prev.next = null; // Nuke only pointers to previous node, because target node is last.
+                targetNode.prev = null;
+            } else { // Target node is in between two nodes, and has pointers going both ways.
+                targetNode.prev.next = targetNode.next; // Connect the previous node and the next node with eachother
+                targetNode.next.prev = targetNode.prev;
+                targetNode.next = null; // Nuke outgoing pointers
+                targetNode.prev = null;
+            }
+
             console.log(`Task ${taskId} has been acknowledged and deleted from the list.`)
             return true; // Return if the ID's are the same
         } else {
-            currentTask = queue.prev; // continue the iteration in the while-loop
+            targetNode = queue.prev; // continue the iteration in the while-loop
         }
     }
 
@@ -111,14 +137,4 @@ function requeue(queue, taskId) {
         console.log(`Task ${taskId} was not found`);
         return false;
     }
-}
-
-function deleteNode(node) {
-    if (node.prev) { // If there is a previous node
-        node.prev.next = node.next; // Set the next of the previous node to the next node of the current node
-    }
-    if (node.next) { // If there is a next node
-        node.next.prev = node.prev; // Set the previous of the next node to the previous node of the current node
-    }
-
 }
