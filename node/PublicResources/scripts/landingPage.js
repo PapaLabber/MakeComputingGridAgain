25f87ec16c4be11ed082443a70468ac44c3b2159
philippeysl20 <rd57yh@student.aau.dk>
2025-05-09 15:25:59 +0200

    testing

--- node/PublicResources/scripts/landingPage.js
+++ node/PublicResources/scripts/landingPage.js
@@ -5,7 +5,6 @@ document.getElementById('save-email-btn').addEventListener('click', function () 
         chrome.storage.local.set({ email }, function () {
             console.log('Email saved in chrome.storage:', email);
         });
-        console.log('Email saved:', email);
     } else {
         console.error('Please enter a valid email.');
     }
